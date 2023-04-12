6699299bfcff43e787e5216b9ab83606054e9bdf
Tiago Oliveira <dev.tiago.oliveira@gmail.com>
2023-04-12 09:20:40 -0300

    first commit

---
+++ src/Entity/Product.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use Symfony\Component\Uid\Uuid;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
+    private ?Uuid $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $qty_available = null;
+
+    #[ORM\Column]
+    private ?int $qty_total = null;
+
+    public function __construct()
+    {
+        $this->id = Uuid::v4();
+    }
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getQtyAvailable(): ?int
+    {
+        return $this->qty_available;
+    }
+
+    public function setQtyAvailable(int $qty_available): self
+    {
+        $this->qty_available = $qty_available;
+
+        return $this;
+    }
+
+    public function getQtyTotal(): ?int
+    {
+        return $this->qty_total;
+    }
+
+    public function setQtyTotal(int $qty_total): self
+    {
+        $this->qty_total = $qty_total;
+
+        return $this;
+    }
+}
